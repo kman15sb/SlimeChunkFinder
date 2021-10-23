9b0dd6a46efb00c09f071c91556dd3bf2a99af05
kman15sb <kman15sb@gmail.com>
2021-10-23 16:23:09 -0700

    30% speed improvement

--- src/main.rs
+++ src/main.rs
@@ -11,31 +11,27 @@ mod constants {
 
 pub fn main() {
     let start = Instant::now();
+    let mut count: i32 = 0;
     for i in 1..constants::MAX_SEARCH {
         let pos = &spiral::next_spiral(i);
         if check_square(pos.to_vec()) == true {
-            println!("Found 2x2 at {:?}", pos)
+            count += 1;
+            println!("Found {}x{} at chunk {:?}", constants::GRID_CHECK_SIZE, constants::GRID_CHECK_SIZE, pos)
         }
     }
     let duration = start.elapsed();
-    println!("{:?}", duration);
-
-    // println!("{}", spiral::next_spiral(0));
-    // println!("{}", slime::is_slime_chunk(611464175010909465, &spiral::next_spiral(0)))
+    println!("Code ran in {:?} and found {} matches", duration, count);
 }
 
 fn check_square(pos: Vec<i32>) -> bool {
     let mut count: i32 = 0;
-    for j in 0..constants::GRID_CHECK_SIZE {
+    'outer: for j in 0..constants::GRID_CHECK_SIZE {
         for k in 0..constants::GRID_CHECK_SIZE {
             if slime::is_slime_chunk(constants::SEED, &Chunk{x: (pos[0]-j) as i32, z: (pos[1]-k) as i32}) == false {
                 count += 1;
+                break 'outer;
             }
         }
     }
-    if count > 0 {
-        return false;
-    } else {
-        return true;
-    }
+    return count < 1
 }
